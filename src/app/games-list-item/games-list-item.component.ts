f8e9bcc7699afba0e297fa9f45171dec9fae3d29
Yousif Sako <w0846786@myscc.ca>
2024-09-25 01:08:41 -0400

    Made games-list-item component html display the games list

--- src/app/games-list-item/games-list-item.component.ts
+++ src/app/games-list-item/games-list-item.component.ts
@@ -1,10 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {Games} from "../shared/models/games";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-games-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './games-list-item.component.html',
   styleUrl: './games-list-item.component.css'
 })
