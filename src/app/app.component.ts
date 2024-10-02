e66e9b1159b9a1c6ebc6eebf0753c3da91c4687e
Yousif Sako <w0846786@myscc.ca>
2024-10-01 22:53:03 -0400

    Moved gamesList to mock-content, Added game-service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,4 @@ import {GamesListComponent} from "./games-list/games-list.component";
 })
 export class AppComponent {
   title = 'yousif-sako-Learning-Angular';
-  gameList: Games[] =
-    [
-    {id: 1, name: "TF2", genre: "Shooter", rating: 7, releaseDate: "12/06/2005"},
-    {id: 1, name: "Tarkov", genre: "Shooter", rating: 7, releaseDate: "12/06/2015"},
-    {id: 2, name: "Dota2", genre: "Moba", rating: 5, releaseDate: "4/06/2015"},
-    {id: 3, name: "CSGO", genre: "Shooter", rating: 3, releaseDate: "2/06/2018"},
-    {id: 4, name: "Apex", genre: "Shooter", rating: 8, releaseDate: "4/06/2022"},
-    {id: 5, name: "COD6", genre: "Shooter", rating: 7, releaseDate: "8/06/2024"},
-    {id: 6, name: "Minecraft", genre: "Survival", rating: 7, releaseDate: "7/06/2009"}
-    ]
-
 }
